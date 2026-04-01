a9cbb7e4914588acdab2f14a5e0aa71e37c72a19
paula-alcantara <paullety.m@gmail.com>
2026-04-01 17:21:50 -0300

    ✨ New Feature: Adição dos componentes Navbar e Footer

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+
+function Navbar() {
+  return (
+    <>
+      <div className="w-full flex justify-center py-4 bg-indigo-900 text-white"
+      >
+        <div className="container flex justify-between text-lg mx-8"
+        >
+          Blog da Paula
+          <div className="flex gap-4"
+          >
+            Postagem
+            Temas
+            Cadastrar tema 
+            Perfil 
+            Sair 
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
